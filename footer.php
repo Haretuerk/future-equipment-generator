9ae82c26e033ee4851267d130a9df6b6b313fcdc
Haretuerk <haretuerk@gmx.net>
2013-03-02 21:55:21 +0100

    new

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!-- FOOTER.PHP BEGINN -->
+		<div id="footer2"><a href="#top">zum Seitenanfang</a></div> 
+		<div id="footer"><a href="<?php echo get_permalink(18); ?>">Impressum</a> | <a href="<?php echo get_permalink(39); ?>">Kontakt</a>
+
+	
+  
+</div> <!-- -->
+
+</body>
+</html>
+
+<!-- FOOTER.PHP ENDE -->
